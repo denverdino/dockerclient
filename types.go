d106dfb052d8c44c74aef338e968e85ef9c7a840
Li Yi <denverdino@gmail.com>
2015-04-30 21:23:43 +0800

    Support Node info for Swarm support

--- types.go
+++ types.go
@@ -217,6 +217,7 @@ func (s *State) StateString() string {
 			return "restarting"
 		}
 		return "running"
+
 	}
 
 	if s.Dead {
@@ -228,6 +229,13 @@ func (s *State) StateString() string {
 	}
 
 	return "exited"
+}
+
+type NodeInfo struct {
+	Name string
+	Id   string
+	Addr string
+	Ip   string
 }
 
 type ImageInfo struct {
@@ -276,6 +284,7 @@ type ContainerInfo struct {
 	ResolvConfPath string
 	Volumes        map[string]string
 	HostConfig     *HostConfig
+	Node           *NodeInfo
 }
 
 type ContainerChanges struct {
