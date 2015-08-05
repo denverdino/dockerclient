ebc90cef6365ab390f829db2c14f20c35d2dc50b
Li Yi <denverdino@gmail.com>
2015-08-05 16:43:22 +0800

    Add HostnamePath and HostsPath to ContainerInfo
    Signed-off-by: Li Yi <denverdino@gmail.com>

--- types.go
+++ types.go
@@ -284,6 +284,8 @@ type ContainerInfo struct {
 		Networks    map[string]*EndpointSettings
 	}
 	SysInitPath    string
+	HostnamePath   string
+	HostsPath      string
 	ResolvConfPath string
 	Volumes        map[string]string
 	HostConfig     *HostConfig
